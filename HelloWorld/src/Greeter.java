c947101b65c7c702c00414edb180fd4fa6bcc4f5
Michael Haak <31259830+Michael9094@users.noreply.github.com>
2020-06-23 13:09:35 -0400

    Make tests compact

--- HelloWorld/src/Greeter.java
+++ HelloWorld/src/Greeter.java
@@ -1,14 +1,14 @@
 public class Greeter {
     public String getMessage(String[] args) {
-        StringBuilder sb = new StringBuilder();
         if (args == null || args.length == 0) {
             return "Hello, world!";
         }
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < args.length; ++i) {
             String name = args[i].length() > 0 ? args[i] : "world";
             if (i == 0) {
                 sb.append("Hello, ").append(name);
-
             } else {
                 sb.append(' ').append(name);
             }
--- HelloWorld/test/GreeterTest.java
+++ HelloWorld/test/GreeterTest.java
@@ -1,43 +1,39 @@
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GreeterTest {
+    private static class TestCase {
+        public String label;
+        public String[] input;
+        public String output;
+
+        public TestCase(String label, String[] input, String output) {
+            this.label = label;
+            this.input = input;
+            this.output = output;
+        }
+    }
+
     @TestFactory
     Stream<DynamicTest> getMessage() {
-        List<String> labels = Arrays.asList(
-                "null",
-                "empty",
-                "blank",
-                "Richard",
-                "Michael",
-                "Anupam"
-        );
-        List<String[]> inputs = Arrays.asList(
-                null,
-                new String[0],
-                new String[]{""},
-                new String[]{"Richard"},
-                new String[]{"Michael"},
-                new String[]{"Anupam", "Preet", "Kaur"}
-        );
-        List<String> outputs = Arrays.asList(
-                "Hello, world!",
-                "Hello, world!",
-                "Hello, world!",
-                "Hello, Richard!",
-                "Hello, Michael!",
-                "Hello, Anupam Preet Kaur!"
-        );
-        return labels.stream().map(label -> DynamicTest.dynamicTest(label, () -> {
-            int idx = labels.indexOf(label);
-            Greeter greeter = new Greeter();
-            assertEquals(outputs.get(idx), greeter.getMessage(inputs.get(idx)));
-        }));
+        List<TestCase> cases = new ArrayList<>(Arrays.asList(
+                new TestCase("null", null, "Hello, world!"),
+                new TestCase("empty", new String[0], "Hello, world!"),
+                new TestCase("blank", new String[]{""}, "Hello, world!"),
+                new TestCase("Richard", new String[]{"Richard"}, "Hello, Richard!"),
+                new TestCase("Michael", new String[]{"Michael"}, "Hello, Michael!"),
+                new TestCase("Anupam", new String[]{"Anupam", "Preet", "Kaur"}, "Hello, Anupam Preet Kaur!")
+        ));
+        Greeter greeter = new Greeter();
+        return cases.stream().map(testCase -> DynamicTest.dynamicTest(testCase.label, () ->
+                assertEquals(testCase.output, greeter.getMessage(testCase.input))
+        ));
     }
 }
